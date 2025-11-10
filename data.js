34e1ca15f7d24dcb3e3496a4bda6f637c99a888a
ziadalkalza <ziadalkalza@gmail.com>
2025-11-10 15:49:46 +0200

    ui-change

---
+++ data.js
@@ -0,0 +1,15 @@
+window.contactData = {
+  name: "Ziad Alkalza",
+  title: "Founder - Aradina | Smart Agriculture & IoT",
+  bio: "Building resilient farm automation systems that fuse sensor networks, AI, and on-ground expertise to unlock sustainable food production.",
+  email: "ziad@aradina.solutions",
+  phone: "+201095556608",
+  phoneLabel: "+20 109 555 6608",
+  linkedinUrl: "https://www.linkedin.com/in/ziad-alkalza-a000a0166/",
+  companyUrl: "https://aradina.solutions",
+  linkedinLabel: "LinkedIn",
+  companyLabel: "Company",
+  vcfPath: "./contact.vcf",
+  viewCardLabel: "View vCard",
+  addToContactsLabel: "Add to Contacts"
+};
